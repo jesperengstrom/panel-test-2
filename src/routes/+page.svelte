1a16a5f6d4064595a7cce5e768863e024987fff9
Jesper Engström <jesper.engstrom@kb.se>
2025-04-26 14:19:02 +0200

    grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="app grid min-h-screen">
+  <header class="header bg-green-100">Header</header>
+  <section class="left-pane bg-gray-100">Left pane</section>
+  <main class="main bg-yellow-50">main</main>
+  <aside class="aside bg-blue-50">Aside</aside>
+</div>
+
+<style>
+  .app {
+    grid-template: 
+    'header header header' 70px
+    'left-pane main aside' auto / 250px auto 250px;
+  }
+
+  .header {
+    grid-area: header;
+  }
+
+  .left-pane {
+    grid-area: left-pane;
+  }
+
+  .main {
+    grid-area: main;
+  }
+
+  aside {
+    grid-area: aside;
+  }
+</style>
