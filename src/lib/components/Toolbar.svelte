589708821c76c57485295920e7d3bcf98a582ea4
Jesper Engström <jesper.engstrom@kb.se>
2025-04-27 17:50:08 +0200

    Add more stuff

---
+++ src/lib/components/Toolbar.svelte
@@ -0,0 +1,7 @@
+<nav class="toolbar bg-white border border-gray-200 h-(--toolbar-height)">toolbar</nav>
+
+<style>
+  .toolbar {
+    grid-area: toolbar;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,80 @@
-<div class="app grid min-h-screen">
-  <header class="header bg-green-100">Header</header>
-  <section class="left-pane bg-gray-100">Left pane</section>
-  <main class="main bg-yellow-50">main</main>
-  <aside class="aside bg-blue-50">Aside</aside>
+<script>
+  import { page } from "$app/state";
+  import Toolbar from "$lib/components/Toolbar.svelte";
+  let leadingPaneWidth = 250;
+  const trailingPaneWidth = $derived.by(() => {
+    let numberOfTrailingPanes = page.url.searchParams.getAll('preview').length;
+    return 250 * numberOfTrailingPanes;
+  })
+</script>
+
+<div class="app grid min-h-screen max-w-screen overflow-x-hidden"
+  style={`grid-template-columns: ${leadingPaneWidth}px auto ${trailingPaneWidth}px`}>
+  <header class="header bg-green-100">appBar</header>
+  <section class="leading-pane hidden md:block bg-gray-100">
+    <Toolbar />
+    leading pane
+  </section>
+  <section class="content grid bg-yellow-50">
+    <Toolbar />
+    <main class="main">
+      <div class="flex flex-col">
+        <a href="/?preview=1">add one right pane</a>
+        <a href="/?preview=1&preview=2">add two right panes</a>
+        <a href="/?preview=1&preview=2&preview=3">add three right panes</a>
+        <a href="/">remove all right panes</a>
+      </div>
+      main
+    </main>
+    <aside class="aside bg-amber-100">aside</aside>
+  </section>
+  <section class="trailing-panes hidden md:block bg-blue-50">
+    <Toolbar />
+    trailing panes
+  </section>
 </div>
 
-<style>
+<style style="postcss">
   .app {
-    grid-template: 
-    'header header header' 70px
-    'left-pane main aside' auto / 250px auto 250px;
+    grid-template-areas:
+    'header header header' 
+    'content content content';
+
+    grid-template-rows: var(--app-bar-height) auto;
+
+    /* variant */
+    @media (width >= 48rem) { 
+      grid-template-areas:
+    'header header header' 
+    'leading-pane content trailing-panes';
+    }
   }
 
   .header {
     grid-area: header;
   }
 
-  .left-pane {
-    grid-area: left-pane;
+  .leading-pane {
+    grid-area: leading-pane;
+  }
+
+  .content {
+    grid-area: content;
+
+    grid-template: 
+    'toolbar toolbar' var(--toolbar-height)
+    'main aside' auto / auto 250px
+  }
+
+  .trailing-panes {
+    grid-area: trailing-panes;
   }
 
   .main {
     grid-area: main;
   }
 
-  aside {
+  .aside {
     grid-area: aside;
   }
 </style>
