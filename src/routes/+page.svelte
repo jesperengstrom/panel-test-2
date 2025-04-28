8f758a42f637c4bf3a72fcfe4e747960cecae415
Jesper Engström <jesper.engstrom@kb.se>
2025-04-28 11:28:06 +0200

    try transition

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,32 @@
 <script>
   import { page } from "$app/state";
   import Toolbar from "$lib/components/Toolbar.svelte";
-  let leadingPaneWidth = 250;
+
+  let leadingPaneClosed = $state(false);
+  let leadingPaneWidth = $derived(leadingPaneClosed ? 0 : 250);
+
   const trailingPaneWidth = $derived.by(() => {
     let numberOfTrailingPanes = page.url.searchParams.getAll('preview').length;
     return 250 * numberOfTrailingPanes;
   })
 </script>
 
-<div class="app grid min-h-screen max-w-screen overflow-x-hidden"
+<div class="app grid min-h-screen max-w-screen overflow-x-hidden transition-all"
   style={`grid-template-columns: ${leadingPaneWidth}px auto ${trailingPaneWidth}px`}>
   <header class="header bg-green-100">appBar</header>
-  <section class="leading-pane hidden md:block bg-gray-100">
+  <section class="leading-pane relative overflow-x-hidden overflow-y-scroll hidden md:block bg-gray-100">
     <Toolbar />
-    leading pane
+    <div class={['absolute w-full transition-all', leadingPaneClosed ? '-translate-x-full' : 'translate-x-0']}>
+      <p>leading pane</p>
+      <ol class="whitespace-nowrap [&_li]:overflow-hidden [&_li]:overflow-ellipsis">
+        <li>leading paneleading paneleading paneleading paneleading paneleading pane</li>
+        <li>leading paneleading paneleading paneleading paneleading paneleading pane</li>
+        <li>leading paneleading paneleading paneleading paneleading paneleading pane</li>
+        <li>leading paneleading paneleading paneleading paneleading paneleading pane</li>
+        <li>leading paneleading paneleading paneleading paneleading paneleading pane</li>
+        <li>leading paneleading paneleading paneleading paneleading paneleading pane</li>
+      </ol>
+    </div>
   </section>
   <section class="content grid bg-yellow-50">
     <Toolbar />
@@ -23,6 +36,7 @@
         <a href="/?preview=1&preview=2">add two right panes</a>
         <a href="/?preview=1&preview=2&preview=3">add three right panes</a>
         <a href="/">remove all right panes</a>
+        <button onclick={() => leadingPaneClosed = !leadingPaneClosed}>Close left pane</button>
       </div>
       main
     </main>
@@ -34,8 +48,9 @@
   </section>
 </div>
 
-<style style="postcss">
+<style>
   @import 'tailwindcss';
+
   .app {
     grid-template-areas:
     'header header header' 
